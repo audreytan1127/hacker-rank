283571862ab28968222ef5db94ad6ac3548a1fa2
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-09-06 11:02:59 -0500

    completed HackerRank Problem Solving Q2 simpleArraySum

--- src/main/java/ProblemSolving/ProblemSolving.java
+++ src/main/java/ProblemSolving/ProblemSolving.java
@@ -1,5 +1,14 @@
 package ProblemSolving;
 
+import java.util.List;
+
 public class ProblemSolving {
+    public static int simpleArraySum(List<Integer> ar) {
+        int sumOfInt = 0;
+        for (int i = 0; i < ar.size(); i++) {
+            sumOfInt += ar.get(i);
+        }
+        return sumOfInt;
+    }
 
 }
