277df8b9d54337b23db5b2eb90331fe579ce9b70
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-11-18 13:31:29 -0600

    completed java hackerrank for print format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,18 +21,46 @@ public class Main {
 //        System.out.println(c);
 
 //        HackerRank Q6: Scanner with Conditions
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        if (N%2!=0){
-            System.out.println("Weird");
-        } else if (N>=2 && N<=5){
-            System.out.println("Not Weird");
-        } else if (N>=6 && N<=20){
-            System.out.println("Weird");
-        } else if(N>20){
-            System.out.println("Not Weird");
-        }
-        scan.close();
+//        Scanner scan = new Scanner(System.in);
+//        int N = scan.nextInt();
+//        if (N%2!=0){
+//            System.out.println("Weird");
+//        } else if (N>=2 && N<=5){
+//            System.out.println("Not Weird");
+//        } else if (N>=6 && N<=20){
+//            System.out.println("Weird");
+//        } else if(N>20){
+//            System.out.println("Not Weird");
+//        }
+//        scan.close();
+
+//  // HackerRank Q7: Scanner Input/Output
+//        Scanner scan = new Scanner(System.in);
+//        int i = scan.nextInt();
+//        double d = scan.nextDouble();
+////        Need another nextLine(); to skip the empty space given after nextInt().
+//        scan.nextLine();
+//        String s = scan.nextLine();
+//
+//        System.out.println("String: " + s);
+//        System.out.println("Double: " + d);
+//        System.out.println("Int: " + i);
+
+//        //HackerRank Q8: Java Loops
+//        int N = 2;
+//        for (int i=1; i<=10; i++){
+//            System.out.printf("%d x %d = %d %n", N, i, N*i);
+//        }
+
+//        // HackerRank Q9: Print Format
+//        System.out.println("================================");
+//        for(int i=0;i<3;i++){
+//            String s1=scan.next();
+//            int x=scan.nextInt();
+//            //Complete this line
+//            System.out.printf("%-15s%03d%n", s1, x);
+//        }
+//        System.out.println("================================");
     }
 
 }
