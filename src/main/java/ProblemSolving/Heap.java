14283975e05d91653cb3b653e8fce42fbb3c571b
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-12-06 15:57:51 -0600

    added heap class with buildHeap, heapify, and heapSort methods with partial algorithm

---
+++ src/main/java/ProblemSolving/Heap.java
@@ -0,0 +1,30 @@
+package ProblemSolving;
+
+public class Heap {
+    int[] arr = {10, 5, 6, 15, 8, 20, 1};
+    int arrSize = 10;
+    int i;
+//buildHeap function
+//    builds heap from array given
+    public void buildHeap(int arr[],int arrSize){
+// loop through length of array and
+//        call heapify to organize array into heap
+    }
+
+//    heapify function
+//    ensures that root is smallest value
+//    left to right child is then small to large
+public void heapify(int arr[], int arrSize, int i) {
+// initialize root as smallest value
+//    initialize parent
+//    initialize left child
+//    initialize right child
+//
+
+}
+//    heapSort function
+//    sorts array from smallest to largest(without using sort())
+    public void heapSort(int arr[], int arrSize){
+
+    }
+}
