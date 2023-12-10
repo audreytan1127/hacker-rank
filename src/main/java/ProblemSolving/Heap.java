54a14fd447f1f3ef9ff53d51212e17d18fe7714d
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-12-10 14:10:50 -0600

    buildHeap method completed in heap class

--- src/main/java/ProblemSolving/Heap.java
+++ src/main/java/ProblemSolving/Heap.java
@@ -4,27 +4,33 @@ public class Heap {
     int[] arr = {10, 5, 6, 15, 8, 20, 1};
     int arrSize = 10;
     int i;
-//buildHeap function
+
+    //buildHeap function
 //    builds heap from array given
-    public void buildHeap(int arr[],int arrSize){
+    public void buildHeap(int arr[], int arrSize) {
+
 // loop through length of array and
+        for (int i = arrSize / 2 - 1; i >= 0; i--) {
 //        call heapify to organize array into heap
+            heapify(arr, arrSize, i);
+        }
     }
 
-//    heapify function
+    //    heapify function
 //    ensures that root is smallest value
 //    left to right child is then small to large
-public void heapify(int arr[], int arrSize, int i) {
+    public void heapify(int arr[], int arrSize, int i) {
 // initialize root as smallest value
 //    initialize parent
 //    initialize left child
 //    initialize right child
 //
 
-}
-//    heapSort function
+    }
+
+    //    heapSort function
 //    sorts array from smallest to largest(without using sort())
-    public void heapSort(int arr[], int arrSize){
+    public void heapSort(int arr[], int arrSize) {
 
     }
 }
