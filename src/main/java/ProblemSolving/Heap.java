496a452415b06b1439adf5cf10df7b13f0787343
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-12-12 20:01:25 -0600

    completed heap class with buildHeap, heapSort, and heapify methods

--- src/main/java/ProblemSolving/Heap.java
+++ src/main/java/ProblemSolving/Heap.java
@@ -21,16 +21,55 @@ public class Heap {
 //    left to right child is then small to large
     public void heapify(int arr[], int arrSize, int i) {
 // initialize root as smallest value
-//    initialize parent
+        int largest = i;
 //    initialize left child
+        int left = 2 * i + 1;
 //    initialize right child
-//
+        int right = 2 * i + 2;
+// check if left child is smaller than parent
+        if (left < arrSize && arr[left] > arr[largest]) {
+//        if so, switch left child with root index
+            largest = left;
+        }
+//        check if right child is smaller than parent
+        if (right < arrSize && arr[right] > arr[largest]) {
+//        if so, switch right child with root index
+            largest = right;
+        }
+//        check if the largest value is at i in the list
+        if (largest != i) {
+//        if not, declare placeholder variable equal to the list at index i
+            int swap = arr[i];
+//        set variable equal to largest value
+            arr[i] = arr[largest];
+//        and set at the index of the largest value equal to placeholder value
+            arr[largest] = swap;
+//            recursively call heapify
+            heapify(arr, arrSize, i);
+        }
 
     }
 
     //    heapSort function
 //    sorts array from smallest to largest(without using sort())
-    public void heapSort(int arr[], int arrSize) {
-
+    public void heapSort(int arr[]) {
+//        declare length of list to array length
+        int arrSize = arr.length;
+//     Builds heap from index of 0
+        for (int i = arrSize / 2 - 1; i >= 0; i--) {
+//            calls heapify with each input of the array
+            heapify(arr, arrSize, i);
+        }
+//        loops through array to make sure that the root value is largest value
+        for(int i = arrSize - 1; i>0; i--){
+//            sets the root value into placeholder variable
+            int temp = arr[0];
+//            sets root index value equal to largest value
+        arr[0] = arr[i];
+//        sets index of new root value to old root value stored in variable
+        arr[i] = temp;
+//        calls heapify to ensure largest value is in root index
+        heapify(arr, i, 0);
+        }
     }
 }
